b345bfa8298a386ce2ff916af56e61b9fb70974f
ShaneChaneShane <wannasut.chane@gmail.com>
2026-05-18 20:28:11 +0700

    feat: custom marker test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 // https://www.geeksforgeeks.org/flutter/how-to-get-users-current-location-on-google-maps-in-flutter/
 // https://fernandoptr.medium.com/how-to-get-users-current-location-address-in-flutter-geolocator-geocoding-be563ad6f66a
+// https://stackoverflow.com/questions/56597739/how-to-customize-google-maps-marker-icon-in-flutter
 
 import 'dart:async';
+// import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const MainApp());
@@ -41,6 +46,124 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Position? _currentPosition;
   Completer<GoogleMapController> _controller = Completer();
+
+  Future<ui.Image> getImageFromPath(String imagePath) async {
+    // File imageFile = File(imagePath);
+
+    ByteData imageBytesBuffer = await rootBundle.load(imagePath);
+    Uint8List imageBytes = imageBytesBuffer.buffer.asUint8List();
+    final Completer<ui.Image> completer = new Completer();
+
+    ui.decodeImageFromList(imageBytes, (ui.Image img) {
+      return completer.complete(img);
+    });
+
+    return completer.future;
+  }
+
+  Future<BitmapDescriptor> getMarkerIcon(String imagePath, Size size) async {
+    final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+
+    final Radius radius = Radius.circular(size.width / 2);
+
+    final Paint tagPaint = Paint()..color = Colors.blue;
+    final double tagWidth = 40.0;
+
+    final Paint shadowPaint = Paint()..color = Colors.blue.withAlpha(100);
+    final double shadowWidth = 15.0;
+
+    final Paint borderPaint = Paint()..color = Colors.white;
+    final double borderWidth = 3.0;
+
+    final double imageOffset = shadowWidth + borderWidth;
+
+    // Add shadow circle
+    canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromLTWH(0.0, 0.0, size.width, size.height),
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
+      ),
+      shadowPaint,
+    );
+
+    // Add border circle
+    canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromLTWH(
+          shadowWidth,
+          shadowWidth,
+          size.width - (shadowWidth * 2),
+          size.height - (shadowWidth * 2),
+        ),
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
+      ),
+      borderPaint,
+    );
+
+    // Add tag circle
+    canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromLTWH(size.width - tagWidth, 0.0, tagWidth, tagWidth),
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
+      ),
+      tagPaint,
+    );
+
+    // Add tag text
+    TextPainter textPainter = TextPainter(textDirection: TextDirection.ltr);
+    textPainter.text = TextSpan(
+      text: '1',
+      style: TextStyle(fontSize: 20.0, color: Colors.white),
+    );
+
+    textPainter.layout();
+    textPainter.paint(
+      canvas,
+      Offset(
+        size.width - tagWidth / 2 - textPainter.width / 2,
+        tagWidth / 2 - textPainter.height / 2,
+      ),
+    );
+
+    // Oval for the image
+    Rect oval = Rect.fromLTWH(
+      imageOffset,
+      imageOffset,
+      size.width - (imageOffset * 2),
+      size.height - (imageOffset * 2),
+    );
+
+    // Add path for oval image
+    canvas.clipPath(Path()..addOval(oval));
+
+    // Add image
+    ui.Image image = await getImageFromPath(imagePath);
+    paintImage(canvas: canvas, image: image, rect: oval, fit: BoxFit.fitWidth);
+
+    // Convert canvas to image
+    final ui.Image markerAsImage = await pictureRecorder.endRecording().toImage(
+      size.width.toInt(),
+      size.height.toInt(),
+    );
+
+    // Convert image to bytes
+    final ByteData? byteData = await markerAsImage.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
+    final Uint8List uint8List = byteData!.buffer.asUint8List();
+    return BitmapDescriptor.bytes(uint8List);
+  }
+
   // on below line we have specified camera position
   static final CameraPosition _kGoogle = const CameraPosition(
     target: LatLng(20.42796133580664, 80.885749655962),
@@ -140,9 +263,7 @@ class _HomePageState extends State<HomePage> {
         onPressed: () async {
           getUserCurrentLocation().then((value) async {
             print(
-              _currentPosition!.latitude.toString() +
-                  " " +
-                  _currentPosition!.longitude.toString(),
+              "${_currentPosition!.latitude.toString()} ${_currentPosition!.longitude.toString()}",
             );
 
             // marker added for current users location
@@ -152,6 +273,10 @@ class _HomePageState extends State<HomePage> {
                 position: LatLng(
                   _currentPosition!.latitude,
                   _currentPosition!.longitude,
+                ),
+                icon: await getMarkerIcon(
+                  "assets/userIconTest.png",
+                  Size(100.0, 100.0),
                 ),
                 infoWindow: InfoWindow(title: 'My Current Location'),
               ),
@@ -173,7 +298,7 @@ class _HomePageState extends State<HomePage> {
             setState(() {});
           });
         },
-        child: Icon(Icons.local_activity),
+        child: Icon(Icons.center_focus_strong_rounded),
       ),
     );
   }
